fd8392c309724bc36ad915e01d04795662282005
Diego Oliveira <diego.araujo234@gmail.com>
2022-11-29 23:41:00 -0300

    Criação de hash equals e toString

--- ContaBanco/src/classes/metodos/Cliente.java
+++ ContaBanco/src/classes/metodos/Cliente.java
@@ -54,6 +54,71 @@ public class Cliente {
 	public void setNomePai(String nomePai) {
 		this.nomePai = nomePai;
 	}
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", sobrenome=" + sobrenome + ", cpf=" + cpf + ", registroGeral="
+				+ registroGeral + ", dataNascimento=" + dataNascimento + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((nomeMae == null) ? 0 : nomeMae.hashCode());
+		result = prime * result + ((nomePai == null) ? 0 : nomePai.hashCode());
+		result = prime * result + ((registroGeral == null) ? 0 : registroGeral.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (dataNascimento == null) {
+			if (other.dataNascimento != null)
+				return false;
+		} else if (!dataNascimento.equals(other.dataNascimento))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nomeMae == null) {
+			if (other.nomeMae != null)
+				return false;
+		} else if (!nomeMae.equals(other.nomeMae))
+			return false;
+		if (nomePai == null) {
+			if (other.nomePai != null)
+				return false;
+		} else if (!nomePai.equals(other.nomePai))
+			return false;
+		if (registroGeral == null) {
+			if (other.registroGeral != null)
+				return false;
+		} else if (!registroGeral.equals(other.registroGeral))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		return true;
+	}
 	
 	
 }
