bfd57f8f1cf57185949a91815177c0cb69b0f14d
Diego Oliveira <diego.araujo234@gmail.com>
2022-11-29 23:37:07 -0300

    Criação de JOPANE para captação de dados e criação de objeto

--- ContaBanco/src/classes/metodos/ClasseExecutavel.java
+++ ContaBanco/src/classes/metodos/ClasseExecutavel.java
@@ -1,14 +1,35 @@
 package classes.metodos;
 
+import javax.swing.JOptionPane;
+
 public class ClasseExecutavel {
 
 	public static void main(String[] args) {
+	String nome =JOptionPane.showInputDialog("Nome do Cliente");
+	String sobrenome =JOptionPane.showInputDialog("Sobrenome do Cliente");	
+	String dataNascimento =JOptionPane.showInputDialog("Data de Nascimento");
+	String cpf =JOptionPane.showInputDialog(" CPF ");
+	String rg =JOptionPane.showInputDialog(" RG ");
+	String nomePai =JOptionPane.showInputDialog("Nome do Pai");
+	String nomeMae =JOptionPane.showInputDialog("Nome do Mãe");
+	String deposito =JOptionPane.showInputDialog("Valor de Depósito para abertura de conta");
+	double d = Double.parseDouble(deposito);
 	Cliente c1 = new Cliente();
-	c1.setNome("Diego");
-	c1.setSobrenome("de Araujo Oliveira");
-	c1.setCpf("123.456.789-00");
-	Metodos c2 = new Metodos("Diego","de Araujo Oliveira","123.456.789-78",200);
-	c2.Extrato();
-	}
+	c1.setNome(nome);
+	c1.setSobrenome(sobrenome);
+	c1.setCpf(cpf);
+	c1.setRegistroGeral(rg);
+	c1.setNomePai(nomePai);
+	c1.setNomeMae(nomeMae);
+	c1.setDataNascimento(dataNascimento);
 
+	Metodos c = new Metodos();
+	c.setCliente(c1);
+	c.depositoAbertura(d);
+	c.Extrato();
+	c.getClientes().add(c1);
+	/*Metodos c2 = new Metodos("Diego","de Araujo Oliveira","123.456.789-78",200);
+	c2.Extrato();
+	}*/
+	}
 }
